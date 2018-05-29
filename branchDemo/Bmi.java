856e9cf586c5b8630b3e9078aba43a02d3c3fa53
HYERI-HONG <rusa12615059@gmail.com>
2018-05-29 17:36:17 +0900

    sixth commit - BMI complete

---
+++ branchDemo/Bmi.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Bmi{
+	public static void main(String[] args){
+		
+		Scanner scan =new Scanner(System.in);
+		System.out.println("\t\t<BMI 계산기>\n\n사용자 이름을 입력하세요 : ");
+		String name = scan.next();
+
+		System.out.println("\n사용자 체중(kg)을 입력하세요 : ");
+		double weight = scan.nextDouble();
+
+		System.out.println("\n사용자 키(cm)를 입력하세요 : ");
+		Double height = scan.nextDouble();
+		height /= 100;
+
+		double result = weight/Math.pow(height,2);
+		String state ="";
+
+		if(result<18.5){
+			state ="저체중";
+		}else if(18.5<=result&&result<23){
+			state ="정상";
+		}else if(23<=result&&result<25){
+			state ="비만 전단계";
+		}else if(25<=result&&result<30){
+			state ="1단계 비만";
+		}else if(30<=result&&result<35){
+			state ="2단계 비만";
+		}else{
+			state ="3단계 비만";
+		}
+		System.out.println("\n\n결과 : "+name+"님은 "+state+"입니다.");
+	}
+}
